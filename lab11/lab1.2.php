60e6a4780804f00d3f1bf6d8bb7e887d58dda9ed
ricardocandelo <richar07disdan@gmail.com>
2022-09-03 15:20:05 -0500

    subir

---
+++ lab11/lab1.2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laboratorio 1.2</title>
+</head>
+<body>
+    <?php
+        $n1 = 1;
+        $n2 = 2;
+        $suma = $n1 + $n2;
+        echo "suma".$suma;
+        echo "<br/>$n1+$n2";
+    ?>
+    <h1></h1>
+</body>
+</html>
