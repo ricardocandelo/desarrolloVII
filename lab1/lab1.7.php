b791485c640e4585b269b2f8c8bb0cec56ad1639
ricardocandelo <richar07disdan@gmail.com>
2022-09-04 16:33:20 -0500

    lab1

--- lab1/lab1.7.php
+++ lab1/lab1.7.php
@@ -4,15 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laboratorio 1.1</title>
+    <title>Manejo de variable</title>
 </head>
 <body>
     <?php
-    $ini = "Hola ";
-    $fin = "a todos";
-    $todo = $ini.$fin;
-    echo $todo;
-    
+        $x = 24;
+        $pi = 3.1416;
+        $animal = "conejo";
+        $saludo = "hola caracola" ;
+        echo $x, "<br>", $pi, "</br>", $animal, "</br>", $saludo
     ?>
 </body>
 </html>
---
+++ lab1/lab1.8.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Manejo de Variables II</title>
+</head>
+<body>
+    <?php
+        $a = 8;
+        $b = 3;
+        echo $a + $b, "<br>";
+        echo $a - $b, "<br>";
+        echo $a * $b, "<br>";
+        echo $a / $b, "<br>";
+        $a++; echo $a, "<br>";
+        $b--; echo $b, "<br>";
+
+    ?>
+</body>
+</html>
