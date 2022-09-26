e8c55dc38f0edac3605e7a3ed4aa73d403ddb0d7
ricardocandelo <richar07disdan@gmail.com>
2022-09-26 14:31:26 -0500

    lab4

---
+++ lab4/lab4.5.php
@@ -0,0 +1,46 @@
+<?php
+  
+      $matriz = array();
+      $sumaColun=0;
+      $sumaFila=0;
+
+
+      if(isset($_POST['enviar'])){
+        $nu= $_POST['cant'];
+        $c=0;
+        for($i=0; $i<$nu; $i++){
+          for($j=0; $j<$nu; $j++){
+            $matriz[$i][$j]=0;
+          }
+        }
+
+
+        if ($nu % 2 == 0){
+          for($i=0; $i<$nu; $i++){
+            for($j=0; $j<$nu; $j++){
+              $matriz[$i][$c]=1;
+            }
+          }
+
+
+          foreach($matriz as $clave=>$valor){
+            echo "<br>";
+            for($i=0; $i<count($matriz); $i++){
+              for($i=0; $i<count($matriz); $i++){
+                echo "&nbsp &nbsp[ ".$valor[$i]." ]&nbsp &nbsp";
+              }
+              echo "<br>";
+            }
+          }
+
+      
+        }else
+        {
+          echo 'Debe ingresar un numero Impar <form action="index.php">
+          <input type="submit"  name="regresar" value="Regresar"> 
+          </form>';
+        } 
+
+      }
+
+?>
